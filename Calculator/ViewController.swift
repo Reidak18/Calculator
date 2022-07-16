ce9f51dfd735a9bd25f308119eb4997bf55ea900
Nikita Lukyantsev <lnik18@mail.ru>
2022-07-16 10:40:53 +0300

    Переименовал функции, добавил стирание последнего символа

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -7,7 +7,8 @@
 
 import UIKit
 
-public enum ActionTag: Int {
+// для преобразования тега в символ математической операции
+public enum OperationTag: Int {
     case Plus = 0
     case Minus = 1
     case Multiplication = 2
@@ -15,6 +16,7 @@ public enum ActionTag: Int {
     case GetResult = 4
 }
 
+// для преобразования тега в спец символ - точка или скобки
 public enum SpecSymbolTag: Int {
     case Point = 0
     case LeftBracket = 1
@@ -23,22 +25,20 @@ public enum SpecSymbolTag: Int {
 
 class ViewController: UIViewController {
     @IBOutlet weak var expression: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-    }
-
+    // ввод числа
     @IBAction func inputDigit(_ sender: UIButton) {
         if expression.text?.last != ")" {
             expression.text! += "\(sender.tag)"
         }
     }
     
-    @IBAction func inputAction(_ sender: UIButton) {
-        guard let tag = ActionTag(rawValue: sender.tag),
-              let lastSymbol = expression.text?.last,
-              lastSymbol.isNumber || lastSymbol == ")"
+    // ввод символа операции
+    @IBAction func inputOperation(_ sender: UIButton) {
+        let lastSymbol: Character = expression.text?.last ?? "\0"
+        guard let tag = OperationTag(rawValue: sender.tag),
+              lastSymbol.isNumber || lastSymbol == ")" ||
+                tag == .Minus && (expression.text!.isEmpty || lastSymbol == "(")
         else { return }
         
         switch(tag) {
@@ -51,12 +51,13 @@ class ViewController: UIViewController {
         case .Division:
             expression.text! += "/"
         case .GetResult:
-            let result = Calculate(expressionStr: expression.text!)
+            let result = calculate(expressionStr: expression.text!)
             expression.text = "\(result)"
             break
         }
     }
     
+    // ввод спец символа
     @IBAction func inputSpec(_ sender: UIButton) {
         guard let tag = SpecSymbolTag(rawValue: sender.tag)
         else { return }
@@ -87,15 +88,25 @@ class ViewController: UIViewController {
         }
     }
     
+    // очистка поля ввода
     @IBAction func clear(_ sender: UIButton) {
         expression.text = ""
+    }
+    
+    // стирание последнего символа
+    @IBAction func backspace(_ sender: Any) {
+        guard var inputText = expression.text
+        else { return }
+        
+        inputText.remove(at: inputText.index(before: inputText.endIndex))
+        expression.text! = inputText
     }
     
     private func isActionSymbol(_ symbol: Character) -> Bool {
         return symbol == "+" || symbol == "-" || symbol == "*" || symbol == "/"
     }
     
-    private func Calculate(expressionStr: String) -> Double {
+    private func calculate(expressionStr: String) -> Double {
         let mathExpression = NSExpression(format: expressionStr)
         return (mathExpression.expressionValue(with: nil, context: nil) as? Double)!
     }
